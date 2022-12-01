af48c5106323f41dce37d7c20a92fcff9c1d258c
Jessica Sai <jessicasai@hotmail.com>
2022-12-01 13:22:00 +0800

    latest

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from datetime import date
-from typing import Union
+# from typing import Union
 import os
 import requests
 
@@ -22,9 +22,11 @@ def read_root():
 # http://127.0.0.1:8000/predict?category=sports&from=2022-11-01&language=en
 @app.get("/predict")
 def predict(date_from: str,             # 2022-11-01 (YYYY-MM-DD)
+            keywords: str,              # e.g world cup / bitcoin
             category: str = 'sports'    # sports
+
 ):
     url = "https://newsapi.org/v2/top-headlines"
-    params = {"apiKey": os.environ['newsapi_API_KEY'], 'category':f'{category}', 'language':'en', 'from':f'{date_from}'}
+    params = {"apiKey": os.environ['newsapi_API_KEY'], 'category':f'{category}', 'language':'en', 'q':f'{keywords}', 'from':f'{date_from}'}
     response = requests.get(url, params=params)
     return response.json()
